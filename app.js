ffefdc3fea5176221d42fd8c29296a1a6f992f5f
zhongs <191492580@qq.com>
2017-11-09 15:06:44 +0800

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var ff = require('./lib/ff');
+var createBase64 = require('./lib/toBase64');
+var del = require('./lib/delete');
+
+var config = {
+    src_path: path.join(__dirname, './mp4/video.mp4'),
+    deleteDirAndFile: path.join(__dirname, './dist/'),
+    save_img_path: path.join(__dirname, './dist/img/'),
+    save_mp3_path: path.join(__dirname, './dist/mp3/')
+}
+
+//删除dist目录
+del.deleteAllSync(config.deleteDirAndFile);
+
+//生成音频文件
+ff.createMp3(config.src_path, config.save_mp3_path);
+
+//生成图片
+ff.createImg(config.src_path, config.save_img_path, {
+    size: '375x667', //生成的图片尺寸
+    frame_rate:10 //一秒内捕获的帧数
+}, () => {
+    //图片转base64
+    createBase64({
+        imgPath: config.save_img_path,
+        savePath: path.join(__dirname, './dist/'),
+        fps:10
+    });
+});
